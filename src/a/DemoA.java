1af0badbba2a9304e1ade740368a1398802ef55f
amant <amant@Hadeel.lan>
2024-01-03 21:48:10 -0500

    Implementation added

---
+++ src/a/DemoA.java
@@ -0,0 +1,18 @@
+package a;
+
+public class DemoA {
+	
+	public static void main (String [] args) {
+		
+		System.out.println("Hadool");
+		
+		
+		
+		
+	}
+//Sprint 2 automation 
+	public static void add(int a,int b) {
+		System.out.println(a+b);
+		
+	}
+}
